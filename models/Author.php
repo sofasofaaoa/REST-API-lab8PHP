5db385f924c51afa8a13379d0f76b60b87b20a8c
sofasofaaoa <ladyrobertus3@gmail.com>
2023-04-26 11:34:05 +0700

    Initial commit

---
+++ models/Author.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "author".
+ *
+ * @property int $id
+ * @property string $fio
+ * @property string $born_at
+ *
+ * @property Book[] $books
+ */
+class Author extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'author';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['fio', 'born_at'], 'required'],
+            [['born_at'], 'safe'],
+            [['fio'], 'string', 'max' => 60],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'fio' => 'Fio',
+            'born_at' => 'Born At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Book::class, ['author_id' => 'id']);
+    }
+}
---
+++ models/Book.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property string $name
+ * @property int $year
+ * @property int $amount
+ *
+ * @property Author $author
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['author_id', 'name', 'year', 'amount'], 'required'],
+            [['author_id', 'year', 'amount'], 'integer'],
+            [['name'], 'string', 'max' => 80],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => Author::class, 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'author_id' => 'Author ID',
+            'name' => 'Name',
+            'year' => 'Year',
+            'amount' => 'Amount',
+        ];
+    }
+
+    /**
+     * Gets query for [[Author]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(Author::class, ['id' => 'author_id']);
+    }
+}
